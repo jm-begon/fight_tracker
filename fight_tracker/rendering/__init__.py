9d89af7d82b0adf7bec4b0513f8a0466dea89018
Jean-Michel Begon <jm.begon@gmail.com>
2023-12-22 11:23:59 +0100

    IPykernel + example notebook

--- fight_tracker/rendering/__init__.py
+++ fight_tracker/rendering/__init__.py
@@ -1,5 +1,6 @@
 from .narrator import Narrator
+from .renderer import Renderer
 from .stream_renderer.renderer import StreamRenderer
 from .table import BoolCell, Table
 
-__all__ = ["Narrator", "StreamRenderer", "Table", "BoolCell"]
+__all__ = ["Narrator", "StreamRenderer", "Table", "BoolCell", "Renderer"]
